6f6548dc55637a3ca8b9f3a2ce6efeef91648efe
Elvira-wan <1003727381@qq.com>
2021-09-07 11:35:35 +0800

    message

---
+++ public/precache-manifest.8d0067bebc24801817edc915ff3b4277.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83e9118467ffe8016daeed6092607449",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78feddd222d6a8b5fed6",
+    "url": "/static/css/2.b941e2b4.chunk.css"
+  },
+  {
+    "revision": "1a70e587ec9578ae47d6",
+    "url": "/static/css/main.f78187b5.chunk.css"
+  },
+  {
+    "revision": "78feddd222d6a8b5fed6",
+    "url": "/static/js/2.21fc23d2.chunk.js"
+  },
+  {
+    "revision": "1a70e587ec9578ae47d6",
+    "url": "/static/js/main.9f63d6d7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9161e238b798c444b732dbbb7420a09e",
+    "url": "/static/media/404.9161e238.png"
+  },
+  {
+    "revision": "ab45504baf89be7c7792b4426c4a6594",
+    "url": "/static/media/bg.ab45504b.jpg"
+  },
+  {
+    "revision": "7ddab8e521e32234e3db42c50794e5cf",
+    "url": "/static/media/logo.7ddab8e5.png"
+  }
+]);
